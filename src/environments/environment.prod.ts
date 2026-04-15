d3624d2d04c8b3f9b33ef36290b3b401fc2e5456
Prince-Steele <princesteele23@gmail.com>
2026-04-14 22:49:55 -0500

    environ prod upd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://your-production-backend.com/api',
-  apiUrl: 'https://your-production-backend.com/api',
+  apiBaseUrl: 'https://cropwiseai-production.up.railway.app',
+  apiUrl: 'https://cropwiseai-production.up.railway.app',
   supabaseUrl: 'https://fxearrcxxcojxpjkciqp.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4ZWFycmN4eGNvanhwamtjaXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxMDcyMTksImV4cCI6MjA5MTY4MzIxOX0.sGfF7KCC_2R9O6R7d3i41kuW9mdXm-YIvRutjH9RdmU'
 };
